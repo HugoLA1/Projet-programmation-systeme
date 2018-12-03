2dd9c55555f74a9a3e8e5802cbc3d006a3b43251
Theogorath <theo.drezenugurlu@viacesi.Fr>
2018-12-03 13:38:55 +0100

    Test for a correct commit

--- MasterChefInfo/MasterChefInfo/Program.cs
+++ MasterChefInfo/MasterChefInfo/Program.cs
@@ -14,6 +14,7 @@ namespace MasterChefInfo
         [STAThread]
         static void Main()
         {
+            //Théo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
