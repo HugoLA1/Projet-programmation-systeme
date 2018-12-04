eb5f270e78cfd0243fb7567b32a49af45469fd3e
Logan06250 <logan.lamouar@viacesi.fr>
2018-12-04 11:19:48 +0100

    Add Squelette of project

---
+++ MasterChefInfo/MasterChefInfo/Model/BD/DataBaseExchanger.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterChefInfo
+{
+    public class DataBaseExchanger
+    {
+        private const String server = "localhost";
+        private const String database = "prosit_5.2.8";
+        private const String uid = "root";  //Ici rien de bien compliqué on récupère les information de connection
+        private const String password = "";
+
+        private const String fullConnectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+        //Pour les concaténé ici sous un format bien précis
+        public MySqlConnection Connection()
+        {
+            return new MySqlConnection(fullConnectionString); //methode retournant la connection a la BDD
+        }
+
+        public DataTable ExecuteRequest(MySqlCommand command) //Methode pour executer une requete a partir d'une SqlCommand
+        {
+            using (MySqlConnection connection = Connection())
+            {
+                connection.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command); //Using ici pour la destruction de la connection aprés utilisation
+                command.Connection = connection;
+                DataTable table = new DataTable();
+                //MessageBox.Show(command.CommandText);  //Trés trés utile ce messagebox pour le debug
+                adapter.Fill(table); //On récupère le résultat de la requete sur la base de donnée sous forme d'une DataTable
+                connection.Close();  //On oublie pas de close la connection /!\ et surtout pas de l'open avant de faire du traitement aussi
+                return table; //On la renvoie la ou la methode a été appelé
+            }
+        }
+
+        public void ExecuteAction(MySqlCommand command)
+        {
+            using (MySqlConnection connection = Connection())
+            {
+                connection.Open();                          //Ducoup pas besoin de récupérer une table ou quoi que ce soit on envoi juste une requete
+                command.Connection = connection;
+                //MessageBox.Show(command.CommandText);     //Trés trés utile ce messagebox pour le debug
+                command.ExecuteNonQuery();
+                connection.Close();  //On oublie pas de close la connection /!\ et surtout pas de l'open avant de faire du traitement aussi
+            }
+        }
+    }
+}
---
+++ MasterChefInfo/MasterChefInfo/Model/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterChefInfo
+{
+    class Model
+    {
+
+        DataBaseExchanger BDD;
+        RequestMapper requestMapper;
+        DinnerRoom dinnerRoom;
+        Kitchen kitchen;
+        Counter counter;
+                
+        public Model()
+        {
+            BDD = new DataBaseExchanger();
+            requestMapper = new RequestMapper();
+            dinnerRoom = new DinnerRoom();
+            kitchen = new Kitchen();
+            counter = new Counter();
+        }
+
+    }
+}
