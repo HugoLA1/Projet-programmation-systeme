b97a0ae5119f881a0b5bb0794524c4302f203d68
HugoLA1 <hugo.leboeuf@viacesi.fr>
2018-12-05 11:46:07 +0100

    Implement WaiterController

--- MasterChefInfo/MasterChefInfo/Controller/DinnerRoom/WaiterController.cs
+++ MasterChefInfo/MasterChefInfo/Controller/DinnerRoom/WaiterController.cs
@@ -2,12 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MasterChefInfo
 {
     class WaiterController
     {
+        Model model;
+        Thread threadW;
+
+        public void CreateThread()
+        {
+            threadW = new Thread(new ThreadStart(WatchLoop));
+            threadW.Start();
+        }
+
+        public void WatchLoop()
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                for (int s = 0; s < model.dinnerRoom.squares.Count; s++)
+                {
+                    for (int l = 0; l < model.dinnerRoom.squares[s].lines.Count; l++)
+                    {
+                        for (int t = 0; t < model.dinnerRoom.squares[s].lines[l].tables.Count; t++)
+                        {
+                            if (model.dinnerRoom.squares[s].lines[l].tables[t].groupClient.dishState == DishState.WaitBreadAndWater)
+                            {
+
+                                PutBreadAndWater(model.dinnerRoom.squares[s].lines[l].tables[t]);
+                            }
+                        }
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
         public void MoveToTable(Table table)
         {
 
