895d48b6f5e18429062f1c8e937e7e0801b0104a
Theogorath <theo.drezenugurlu@viacesi.Fr>
2018-12-07 11:39:01 +0100

    Resize character size for a correct usage

--- MasterChefInfo/MasterChefInfo/Form1.cs
+++ MasterChefInfo/MasterChefInfo/Form1.cs
@@ -22,6 +22,22 @@ namespace MasterChefInfo
             MainDrawingArea.BackgroundImageLayout = ImageLayout.Stretch;
             mySpriteController = new SpriteController(MainDrawingArea);
 
+            SpriteLibrary.Sprite sectionChef = new SpriteLibrary.Sprite(new Point(0, 50), mySpriteController,
+                Properties.Resources.ChefMale, 50, 50, 500, 3);
+            sectionChef.SetName("Chef de section");
+            sectionChef.SetSize(new Size(24, 24));
+            sectionChef.PutBaseImageLocation(new Point(240, 240));
+            sectionChef.AutomaticallyMoves = true;
+            sectionChef.CannotMoveOutsideBox = true;
+            //sectionChef.SetSpriteDirectionDegrees(180);
+            sectionChef.MovementSpeed = 2;
+            sectionChef.MoveTo(new Point(72, 239));
+            sectionChef.MoveTo(new Point(72, 192));
+            sectionChef.ChangeAnimation(3);
+            sectionChef.AddAnimation(new Point(50, 150), Properties.Resources.ChefMale, 50, 50, 500, 1);
+            sectionChef.ChangeAnimation(1);
+
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
